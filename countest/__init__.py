b7130b0c519c177d691778ba29845ccf6fb8f638
Sanskriti Singh <108371388+sanskriti-02@users.noreply.github.com>
2024-09-19 12:55:46 +0400

    Create __init__.py

---
+++ countest/__init__.py
@@ -0,0 +1,18 @@
+"""
+A Python Library for Count-Based Classifier Implementation.
+
+``CountEst`` is a library containing the implementation of count-based 
+estimators. For more information regarding how the estimators work, 
+read more in the :ref:`User Guide <user_guide>`.
+
+Subpackages
+----------
+classifiers: Contains the implementation of count-based classifiers 
+    (currently only for categorical data).
+
+"""
+
+# list of all modules available in the library
+__all__ = ['classifiers']
+
+__version__ = '0.1.4'
